ed75a27bbba596ea404f9545c6f39056b6166aef
Johan Angelstam <johan@angelstam.se>
2024-08-21 12:21:42 +0200

    Use ToDo component in App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
+import ToDo from './Components/ToDo.tsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <ToDo></ToDo>
     </>
   )
 }
